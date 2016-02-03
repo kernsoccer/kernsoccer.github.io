15a1b3c586054d93a7b5f6c46e37b494b6956a0e
Jan Steffens <jan.steffens@netzkern.de>
2016-02-03 17:45:46 +0100

    added gamepad support

--- js/game.js
+++ js/game.js
@@ -15,12 +15,11 @@ engine.render.canvas.width = 1920;
 engine.render.canvas.height = 1080;
 engine.world.gravity.y = 0;
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
 
 var circle = Bodies.circle(450,300,20,{
   restitution: 0.4,
+  mass: 10,
+  intertia: Number.POSITIVE_INFINITY,
   render: {
     fillStyle: "red",
     strokeStyle: "black",
@@ -51,7 +50,26 @@ Matter.Body.applyForce(circle, circle.position, Matter.Vector.create(0.01, 0.01)
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, circle, circle2, ball, ground]);
+World.add(engine.world, [circle, circle2, ball, ground]);
 
 // run the engine
 Engine.run(engine);
+
+function update() {
+  var gamepads = navigator.getGamepads();
+  var pad = gamepads[0]
+  if (pad !== undefined) {
+    var x = pad.axes[0];
+    var y = pad.axes[1];
+    var vect = Matter.Vector.create(x,y);
+    if (Matter.Vector.magnitude(vect) > 0.1) {
+      console.log(vect);
+      Matter.Body.applyForce(circle, circle.position, Matter.Vector.mult(vect,0.01));
+    }
+    Bodies
+
+  }
+  requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
