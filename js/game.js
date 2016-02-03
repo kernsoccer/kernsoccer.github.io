c3c16bb51d102fa5e322f7e051869943e56bfec7
deeph00n <jan-steffens@live.com>
2016-02-03 21:34:46 +0100

    start on playingfield

--- js/game.js
+++ js/game.js
@@ -9,35 +9,74 @@ var engine = Engine.create({
     canvas: document.getElementById("playingField")
   }
 });
-engine.render.options.wireframes = false;
+
+engine.render.options.wireframes = true;
 engine.render.options.background = "#007500";
 engine.render.canvas.width = 1920;
 engine.render.canvas.height = 1080;
 engine.world.gravity.y = 0;
 
 
-var circle = Bodies.circle(450,300,20,{
-  restitution: 0.4,
-  mass: 10,
-  intertia: Number.POSITIVE_INFINITY,
-  render: {
-    fillStyle: "red",
-    strokeStyle: "black",
-    lineWidth: 4
-  }
-});
+// CATEGORIES:
+var CAT_BALL = 1;
+var CAT_PLAYER = 2;
+var CAT_RESTRICT_BALL = 4;
 
-var circle2 = Bodies.circle(500,250,20,{
-  restitution: 0.4,
-  render: {
-    fillStyle: "blue",
-    strokeStyle: "black",
-    lineWidth: 4
-  }
-});
+// init game field
+World.add(engine.world, [
+  Bodies.rectangle(1920/2, 10, 1920, 20, { isStatic: true }),
+  Bodies.rectangle(1920/2, 1070, 1920, 20, { isStatic: true }),
+  Bodies.rectangle(10, 1080/2, 20, 1080, { isStatic: true }),
+  Bodies.rectangle(1910, 1080/2, 20, 1080, { isStatic: true }),
+  Bodies.rectangle(1920/2, 50, 1920, 100, {
+    isStatic: true,
+    collisionFilter: {
+      category: CAT_RESTRICT_BALL,
+      mask: CAT_BALL
+    },
+    render: {
+      visible: engine.render.options.wireframes
+    }
+  }),
+  Bodies.rectangle(1920/2, 1030, 1920, 100, {
+    isStatic: true,
+    collisionFilter: {
+      category: CAT_RESTRICT_BALL,
+      mask: CAT_BALL
+    },
+    render: {
+      visible: engine.render.options.wireframes
+    }
+  }),
+  Bodies.rectangle(50, 1080/2, 100, 1080, {
+    isStatic: true,
+    collisionFilter: {
+      category: CAT_RESTRICT_BALL,
+      mask: CAT_BALL
+    },
+    render: {
+      visible: engine.render.options.wireframes
+    }
+  }),
+  Bodies.rectangle(1870, 1080/2, 100, 1080, {
+    isStatic: true,
+    collisionFilter: {
+      category: CAT_RESTRICT_BALL,
+      mask: CAT_BALL
+    },
+    render: {
+      visible: engine.render.options.wireframes
+    }
+  })
+]);
 
 var ball = Bodies.circle(700,500,15,{
   restitution: 0.4,
+  intertia: Number.POSITIVE_INFINITY,
+  collisionFilter: {
+    category: CAT_BALL,
+    mask: CAT_PLAYER | CAT_RESTRICT_BALL
+  },
   render: {
     fillStyle: "white",
     strokeStyle: "black",
@@ -45,12 +84,42 @@ var ball = Bodies.circle(700,500,15,{
   }
 });
 
-Matter.Body.applyForce(circle, circle.position, Matter.Vector.create(0.01, 0.01));
+var circle = Bodies.circle(450,300,20,{
+  restitution: 0.4,
+  mass: 10,
+  frictionAir: 0.1,
+  intertia: Number.POSITIVE_INFINITY,
+  render: {
+    fillStyle: "red",
+    strokeStyle: "black",
+    lineWidth: 4
+  },
+  collisionFilter: {
+    category: CAT_PLAYER,
+    mask: CAT_BALL | CAT_PLAYER
+  }
+});
 
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var circle2 = Bodies.circle(500,250,20,{
+  restitution: 0.4,
+  mass: 10,
+  frictionAir: 0.1,
+  intertia: Number.POSITIVE_INFINITY,
+  render: {
+    fillStyle: "blue",
+    strokeStyle: "black",
+    lineWidth: 4
+  },
+  collisionFilter: {
+    category: CAT_PLAYER,
+    mask: CAT_BALL | CAT_PLAYER
+  }
+});
 
 // add all of the bodies to the world
-World.add(engine.world, [circle, circle2, ball, ground]);
+World.add(engine.world, [circle, circle2, ball]);
+
+
 
 // run the engine
 Engine.run(engine);
@@ -63,11 +132,14 @@ function update() {
     var y = pad.axes[1];
     var vect = Matter.Vector.create(x,y);
     if (Matter.Vector.magnitude(vect) > 0.1) {
-      console.log(vect);
       Matter.Body.applyForce(circle, circle.position, Matter.Vector.mult(vect,0.01));
     }
-    Bodies
-
+    if (pad.buttons[0].pressed) {
+      circle.render.strokeStyle = "white";
+    }
+    else {
+      circle.render.strokeStyle = "black";
+    }
   }
   requestAnimationFrame(update);
 }
