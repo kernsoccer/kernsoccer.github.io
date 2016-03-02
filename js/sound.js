2889f8a818a8c2873e2398b423ee3a2b6a0f53ee
deeph00n <jan-steffens@live.com>
2016-03-02 21:12:10 +0100

    fixed cheer sound name

--- js/sound.js
+++ js/sound.js
@@ -3,14 +3,14 @@ var Sound = function () {
   createjs.Sound.registerSound("snd/kick2.mp3", "kick2");
   createjs.Sound.registerSound("snd/kick3.mp3", "kick3");
   createjs.Sound.registerSound("snd/kick4.mp3", "kick4");
-  createjs.Sound.registerSound("snd/cheer.mp3", "cheer");
+  createjs.Sound.registerSound("snd/cheer1.mp3", "cheer1");
 
   function playKick() {
     createjs.Sound.play(Matter.Common.choose(["kick1","kick2","kick3","kick4"]));
   }
 
   function playCheer() {
-    createjs.Sound.play("cheer");
+    createjs.Sound.play("cheer1");
   }
 
   return {
