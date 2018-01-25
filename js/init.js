667c298e64976db3637253c418784aa6723a774d
Jan Steffens <jan.steffens@netzkern.de>
2018-01-25 18:33:10 +0100

    added netzkern CI theme added it to body for direct start

--- js/init.js
+++ js/init.js
@@ -2,5 +2,6 @@ var game = Game();
 game.init();
 //game.start({"allowDraw":true,"goalLimit":"5","timeLimit":180,"players":[{"gamePadIndex":0,"team":"red","pawnCount":1},{"gamePadIndex":1,"team":"blue","pawnCount":1}]});
 if(window.location.href.indexOf('?start') != -1) {
+    document.body.classList.add('theme-netzkern');
     game.start({"allowDraw":true,"goalLimit":"5","timeLimit":180,"players":[{"gamePadIndex":0,"team":"red","pawnCount":1},{"gamePadIndex":1,"team":"blue","pawnCount":1}]});
 }
